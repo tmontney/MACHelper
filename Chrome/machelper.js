f330c1c748df273ff8985a7eb08bcac22bc35ecd
Tyler Montney <tyler.montney@wast3dgeneration.com>
2020-01-19 21:00:41 -0600

    initial commit

---
+++ Chrome/machelper.js
@@ -0,0 +1,51 @@
+document.addEventListener('paste', onPaste);
+
+function onPaste(event) {
+    var paste = (event.clipboardData || window.clipboardData).getData('text');
+    var delimExp = new RegExp('^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$');
+    var expMatches = delimExp.exec(paste);
+    var firstMatch = null;
+    if (expMatches == null) {
+        var nodelimExp = new RegExp('^([0-9A-Fa-f]{12})$');
+        expMatches = nodelimExp.exec(paste);
+
+        if (expMatches !== null) {
+            firstMatch = expMatches[0];
+        }
+    } else {
+        firstMatch = expMatches[0];
+    }
+
+    if (firstMatch !== null) {
+        chrome.storage.sync.get({
+            sitePreferences: [],
+            paste: paste
+        }, function (data) {
+            var paste = data.paste;
+
+            for (var i = 0; i < data.sitePreferences.length; i++) {
+                var result = data.sitePreferences[i];
+                if (document.URL.indexOf(result.url) !== -1) {
+                    if (result.type == "0") {
+                        paste = paste.replace("-", "").replace(":", "")
+                        break;
+                    } else if (result.type == "1") {
+                        paste = paste.replace("-", "").replace(":", "")
+                        paste = paste.slice(0, 2) + "-" + paste.slice(2, 4) + "-" + paste.slice(4, 6) + "-" + paste.slice(6, 8) + "-" + paste.slice(8, 10) + "-" + paste.slice(10, 12);
+                        break;
+                    } else {
+                        paste = paste.replace("-", "").replace(":", "")
+                        paste = paste.slice(0, 2) + ":" + paste.slice(2, 4) + ":" + paste.slice(4, 6) + ":" + paste.slice(6, 8) + ":" + paste.slice(8, 10) + ":" + paste.slice(10, 12);
+                        break;
+                    }
+                }
+            }
+
+            if (document.activeElement.value) {
+                document.activeElement.value = document.activeElement.value.replace(data.paste, paste);
+            } else {
+                document.activeElement.textContent = document.activeElement.textContent.replace(data.paste, paste);
+            }
+        });
+    }
+}
---
+++ Chrome/options.js
@@ -0,0 +1,100 @@
+function addSitePreference(url, type, addToStorage) {
+  var urlDropdown = document.getElementById("urls");
+
+  var option = document.createElement("option");
+  option.text = url;
+  option.value = type;
+
+  urlDropdown.appendChild(option);
+
+  if (urlDropdown.length > 0) {
+    urlDropdown.selectedIndex = 0;
+  }
+
+  if (addToStorage) {
+    chrome.storage.sync.get({
+      sitePreferences: []
+    }, function (data) {
+      var pref = {};
+      pref["url"] = url;
+      pref["type"] = type;
+      data.sitePreferences.push(pref);
+
+      chrome.storage.sync.set({
+        sitePreferences: data.sitePreferences
+      });
+    });
+  }
+}
+
+function removeSitePreference(url) {
+  chrome.storage.sync.get({
+    sitePreferences: []
+  }, function (data) {
+    for (var i = 0; i < data.sitePreferences.length; i++) {
+      if (data.sitePreferences[i]["url"] == url) {
+        data.sitePreferences.splice(i);
+        break;
+      }
+    }
+
+    chrome.storage.sync.set({
+      sitePreferences: data.sitePreferences
+    });
+  });
+}
+
+function restoreOptions() {
+  chrome.storage.sync.get({
+    sitePreferences: []
+  }, function (data) {
+    var sitePrefs = data.sitePreferences;
+    var urlDropdown = document.getElementById("urls");
+    for (var i = 0; i < urlDropdown.options.length; i++) {
+      urlDropdown.options[i].remove();
+    }
+    for (var i = 0; i < sitePrefs.length; i++) {
+      addSitePreference(sitePrefs[i]["url"], sitePrefs[i]["type"], false);
+    }
+
+    if(sitePrefs.length > 0){
+      urlDropdown.selectedIndex = 0;
+      document.getElementById("urlMacType").selectedIndex = urlDropdown.options[0].value;
+    }
+  });
+}
+
+function addUrl() {
+  var url = document.getElementById("addUrlTXT");
+  var type = document.getElementById("urlMacType");
+  if (url.value !== null && type.selectedIndex !== -1) {
+    addSitePreference(url.value, type.value, true);
+  }
+}
+
+function removeUrl() {
+  var urls = document.getElementById("urls");
+  var selectedUrl = null;
+  if (urls.selectedIndex !== -1) {
+    selectedUrl = urls.options[urls.selectedIndex].text;
+    urls.options[urls.selectedIndex].remove();
+
+    removeSitePreference(selectedUrl);
+  }
+}
+
+function selectUrl() {
+  var urls = document.getElementById("urls");
+  if (urls.selectedIndex !== -1) {
+    var types = document.getElementById("urlMacType");
+    var type = urls[urls.selectedIndex].value;
+    types.selectedIndex = parseInt(type);
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+
+document.getElementById("urls").addEventListener("change", selectUrl);
+document.getElementById("addUrl").addEventListener("click", addUrl);
+document.getElementById("removeUrl").addEventListener("click", removeUrl);
